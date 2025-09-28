1f7a5305895bc2b7481957ee0581a98d95db420b
erwin <ed.cachero@taktylstudios.com>
2025-09-28 16:46:10 +0800

    PlatformParallax added

---
+++ assets/Scripts/PlatformParallax.ts
@@ -0,0 +1,29 @@
+import { _decorator, CCInteger, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('PlatformParallax')
+export class PlatformParallax extends Component {
+    @property({ type: CCInteger })
+    public speed: number = 50;
+
+    @property({ type: CCInteger })
+    public minPos: number = -320;
+
+    @property({ type: CCInteger })
+    public resetPos: number = 640;
+
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        let _currentPos = this.node.getPosition();
+        
+        if (_currentPos.x >= this.minPos) {
+            this.node.setPosition(_currentPos.x -= this.speed * deltaTime, _currentPos.y);
+        } else {
+            this.node.setPosition(this.resetPos, 0);       
+        }
+    }
+}
+
